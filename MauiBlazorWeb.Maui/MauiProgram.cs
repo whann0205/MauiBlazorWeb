042eae206694576f33ff59f013cc5250fe6fa5ad
whann0205 <71807453+whann0205@users.noreply.github.com>
2024-08-23 08:10:43 +0200

    Add project files.

---
+++ MauiBlazorWeb.Maui/MauiProgram.cs
@@ -0,0 +1,36 @@
+using MauiBlazorWeb.Shared;
+using Microsoft.Extensions.Logging;
+using MauiBlazorWeb.Maui.Services;
+using MauiBlazorWeb.Shared.Interfaces;
+using MudBlazor.Services;
+
+namespace MauiBlazorWeb.Maui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            InteractiveRenderSettings.ConfigureBlazorHybridRenderModes();
+
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton<IFormFactor, FormFactor>();
+            builder.Services.AddMudServices();
+
+            return builder.Build();
+        }
+    }
+}
